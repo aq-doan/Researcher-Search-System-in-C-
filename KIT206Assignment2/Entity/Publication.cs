b0a00a2403683ab1b66f4282701c599006ef4dda
aqdoan <billydoananhquan@gmail.com>
2023-04-28 11:36:31 +1000

    initia;

---
+++ KIT206Assignment2/Entity/Publication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206Assignment2.Entity
+{
+    enum OutputType
+    {
+        Journal,
+        Conference,
+        Other
+    }
+    public class Publication
+    {
+        public string DOI { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public DateTime Year { get; set; }
+        public DateTime Available { get; set;}
+   
+
+    }
+}
---
+++ KIT206Assignment2/Entity/Researcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206Assignment2.Entity
+{
+    public class Researcher
+    {
+        public int id { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Title { get; set; }
+        public string School { get; set; }
+        public string campus { get; set; }
+        public string email { get; set; }
+        public string photo { get; set; }
+    }
+}

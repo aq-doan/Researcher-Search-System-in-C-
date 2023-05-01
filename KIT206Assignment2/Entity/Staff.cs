c249e321d9123a5998da3950e350723413dea0b6
Anh Doan <Anh.Doan@utas.edu.au>
2023-05-01 23:19:22 +1000

    implementinG THREE YEAR AVERAGE

--- KIT206Assignment2/Entity/Staff.cs
+++ KIT206Assignment2/Entity/Staff.cs
@@ -8,28 +8,56 @@ namespace KIT206Assignment2.Entity
 {
     public class Staff : Researcher
     {
-        public List<Position> positions {get; set;}
-        /*public float requirement
-        {
-            get 
-            {
-                switch (Job)
-                {
-                    case EmploymentLevel.Student:
-                        return 0;
-                    case EmploymentLevel.A:
-                        return 0.5;
-                    case EmploymentLevel.B:
-                        return 1;
-                    case EmploymentLevel.C:
-                        return 2;
-                    case EmploymentLevel.D:
-                        return 3.2;
-                    case EmploymentLevel.E:
-                        return 4;
+        
+            public List<Position> Positions { get; set; }
+            public List<String> Supervisions { get; set; }
 
+            private float PublicationRequirement
+            {
+                get
+                {
+                    switch (Job)
+                    {
+                        case EmploymentLevel.Student:
+                        return 0F;
+                        case EmploymentLevel.A: 
+                        return 0.5F;
+                        case EmploymentLevel.B:
+                        return 1F;
+                        case EmploymentLevel.C: 
+                        return 2F;
+                        case EmploymentLevel.D:
+                        return 3.2F;
+                        case EmploymentLevel.E:
+                        return 4F;
+                        default: 
+                        return 0;
+                    }
                 }
-            }*/
+            }
+
+            
+            public float ThreeYearAverage
+            {
+                get
+                {
+                    int count = 0;
+                    int currentYear = DateTime.Today.Year;
+
+                    // caculate number of publication from last three year
+                    for (int i = 0; i < PublicationList.Count; i++)
+                    {
+                        if (currentYear - 3 <= PublicationList[i].Year
+                        && PublicationList[i].Year < currentYear)
+                        {
+                            count++;
+                        }
+                    }
+                    return (float)(count / 3.0); //TODO
+                }
+            }
+
+            public float Performance { get { return ThreeYearAverage / PublicationRequirement; } }
         }
-    }
+    
 

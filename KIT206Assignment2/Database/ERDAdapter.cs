21aba7200b6e014c43e1537cf46a14981a2d85d1
Anh Doan <Anh.Doan@utas.edu.au>
2023-05-03 23:12:41 +1000

    Create file for adapter

---
+++ KIT206Assignment2/Database/ERDAdapter.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KIT206Assignment2.Entity;
+using MySql.Data.MySqlClient;
+
+using System.Reflection;
+using System.ComponentModel;
+namespace KIT206Assignment2.Database
+{ 
+}
